6d5232323b00a7916b3f4b06083d2cf2a254ddfc
AdamPandey <N00234646@iadt.ie>
2025-11-09 21:45:04 +0000

    Fix: Definitively repair models/index.js to prevent startup crash

--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
-// models/index.js
 const { Sequelize } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
 
@@ -7,13 +6,22 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// Import models
-db.user = require('./user.model')(sequelize, Sequelize);
-db.world = require('./world.model')(sequelize, Sequelize);
-db.gameSession = require('./gameSession.model')(sequelize, Sequelize);
-db.generationMetric = require('./generationMetric.model')(sequelize, Sequelize); // <-- FIX
-db.worldAsset = require('./worldAsset.model.js')(sequelize, Sequelize);       // <-- FIX
-db.worldContent = require('./worldContent.model.js'); // <-- FIX (It's a Mongoose model, doesn't need sequelize passed in)
+// --- FIX 1: Import all models with their correct names ---
+db.user = require('./user.model.js')(sequelize, Sequelize);
+db.world = require('./world.model.js')(sequelize, Sequelize);
+db.gameSession = require('./gameSession.model.js')(sequelize, Sequelize);
+db.generationMetric = require('./generationMetric.model.js')(sequelize, Sequelize);
+db.worldAsset = require('./worldAsset.model.js')(sequelize, Sequelize);
 
+// --- FIX 2: Import the Mongoose model correctly, without calling it as a function ---
+db.worldContent = require('./worldContent.model.js');
+
+// --- FIX 3: Add this loop to automatically run all the '.associate' functions ---
+// This builds the relationships (e.g., a User has many Worlds)
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 module.exports = db;
