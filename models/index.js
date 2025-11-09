9133c8ec42931f717baaa7c3440e2ccc2f1ee23b
AdamPandey <N00234646@iadt.ie>
2025-11-09 21:59:04 +0000

    Final attempt: Forcing clean rewrite of models/index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+// models/index.js
 const { Sequelize } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
 
@@ -6,20 +7,19 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// --- FIX 1: Import all models with their correct names ---
+// --- Import all Sequelize models correctly ---
 db.user = require('./user.model.js')(sequelize, Sequelize);
 db.world = require('./world.model.js')(sequelize, Sequelize);
 db.gameSession = require('./gameSession.model.js')(sequelize, Sequelize);
 db.generationMetric = require('./generationMetric.model.js')(sequelize, Sequelize);
 db.worldAsset = require('./worldAsset.model.js')(sequelize, Sequelize);
 
-// --- FIX 2: Import the Mongoose model correctly, without calling it as a function ---
+// --- Import the Mongoose model correctly ---
 db.worldContent = require('./worldContent.model.js');
 
-// --- FIX 3: Add this loop to automatically run all the '.associate' functions ---
-// This builds the relationships (e.g., a User has many Worlds)
+// --- Run the model associations to build relationships ---
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
+  if (db[modelName] && db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
