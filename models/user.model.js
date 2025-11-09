ad3cc6749e34891e342f13d79865a73740d79bb6
AdamPandey <N00234646@iadt.ie>
2025-11-09 22:12:01 +0000

    Fix: Correct user model primary key to UUID

--- models/user.model.js
+++ models/user.model.js
@@ -2,9 +2,10 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define('user', {
     id: {
-      type: Sequelize.INTEGER,
+      // FIX: The ID must be a UUID to match the other tables.
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4, // This tells the DB to auto-generate the UUID string
       primaryKey: true,
-      autoIncrement: true
     },
     email: {
       type: Sequelize.STRING,
@@ -19,8 +20,15 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING
     }
   }, {
+    tableName: 'users', // It's good practice to name the table
     timestamps: true
   });
+
+  // It's also good practice to define both sides of the relationship
+  User.associate = (models) => {
+    User.hasMany(models.world, { foreignKey: 'userId' });
+    User.hasMany(models.gameSession, { foreignKey: 'userId' });
+  };
 
   return User;
 };
