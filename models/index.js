c164f7cea67a9f1fc279767e1705755aceb1414a
AdamPandey <N00234646@iadt.ie>
2025-11-09 20:58:38 +0000

    fix: update models/index.js to export all models

--- models/index.js
+++ models/index.js
@@ -10,5 +10,10 @@ db.sequelize = sequelize;
 // Import models
 db.user = require('./user.model')(sequelize, Sequelize);
 db.world = require('./world.model')(sequelize, Sequelize);
+db.gameSession = require('./gameSession.model')(sequelize, Sequelize);
+db.gameSession = require('./generationMetric.model')(sequelize, Sequelize);
+db.gameSession = require('./worldAsset.model.js')(sequelize, Sequelize);
+db.gameSession = require('./worldContent.model.js')(sequelize, Sequelize);
+
 
 module.exports = db;
