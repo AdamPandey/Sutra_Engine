b3acd66a1cc2b02edfeb48d28f3dda21777630ce
AdamPandey <N00234646@iadt.ie>
2025-11-10 09:51:23 +0000

    Fix: Correct DB import in auth middleware

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -1,6 +1,8 @@
-// middleware/authJwt.js
 const jwt = require("jsonwebtoken");
-const db = require("../config/db");
+// --- THIS IS THE FIX ---
+// It was importing from '../config/db', which does not have the models.
+// It MUST import from '../models'.
+const db = require("../models");
 const User = db.user;
 
 const verifyToken = (req, res, next) => {
@@ -11,7 +13,6 @@ const verifyToken = (req, res, next) => {
 
   const token = authHeader.split(" ")[1];
 
-  // FIXED: Use process.env.JWT_SECRET
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).send({ message: "Unauthorized!" });
